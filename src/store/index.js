e35067b24b8034db2550bdc4372a28fa956b7915
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-01-29 21:46:36 +0100

    Refactor tu use middlewares

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
-import { combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { thunk } from 'redux-thunk';
+import * as actionsCreators from './actions.js';
 import * as reducers from './reducers.js';
+
+const composeEnhancers = composeWithDevTools({ actionsCreators });
+const middleware = [thunk];
 export default function configureStore(preloadedState) {
   const store = createStore(
     combineReducers(reducers),
     preloadedState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(...middleware))
   );
   return store;
 }

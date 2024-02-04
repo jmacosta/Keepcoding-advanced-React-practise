cce0a2867f428ae8485e83c76760c52f78b62ed3
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-02-04 13:02:19 +0100

    FIX: apply jest configuration

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
+  baseURL: process.env.VITE_API_BASE_URL
 });
 client.interceptors.response.use(
   response => response.data,
